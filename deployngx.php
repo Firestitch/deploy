c326030ae277f9f6879305f36356c821b30fd111
Raymond <ray@firestitch.com>
2018-01-05 08:57:51 -0500

    moved update.php before frontend

--- deployngx.php
+++ deployngx.php
@@ -42,10 +42,9 @@
 		            'cd ../ && git submodule update --init 2>&1',
 		            'cd ../ && git submodule update --init --remote --merge deploy 2>&1',
 		            'cd ../ && git status 2>&1',
+		            'cd ../backend/command && php upgrade.php 2>&1',
+		            'cd ../backend/command && php init.php 2>&1'
 		            'cd ../frontend && npm install 2>&1'];
-
-	$commands[] = 'cd ../backend/command && php upgrade.php 2>&1';
-	$commands[] = 'cd ../backend/command && php init.php 2>&1';
 
 	$commands = array_merge($commands,
 	                      [  'cd ../frontend && ng build'.($env ? ' --env='.$env : '').' 2>&1',
