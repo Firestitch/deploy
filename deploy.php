6028c0b05e151e47fd72abc7d1ed07d18e3935a5
Raymond <ray@firestitch.com>
2018-03-07 14:19:18 -0500

    added build zip

--- deploy.php
+++ deploy.php
@@ -3,39 +3,36 @@
 
 	$environment	= value($_GET,"environment","development");
 	$branch 		= value($_GET,"branch");
-	$output 		= value($_GET,"output");
+	$action			= value($_GET,"action","build");
+	$output			= $action=="build";
+	$title			= "Building ".ucwords($environment);
 	$minify 		= value($_GET,"minify")=="true";
+	$branch 		= $branch ? $branch : shell_exec("cd ../ && git rev-parse --abbrev-ref HEAD");
 
-	if($output=="zip") {
+	$commands = [ 	is_os_windows() ? "cd" : "echo \$PWD",
+		            is_os_windows() ? "echo %PATH%" : "echo \$PATH",
+		            "git --version",
+		            "cd ../ && git fetch --all",
+		            "cd ../ && git reset --hard origin/".$branch,
+		            "cd ../ && git pull",
+		            "cd ../ && git submodule foreach --recursive git reset --hard",
+		            "cd ../ && git submodule update --init",
+		            "cd ../ && git submodule update --init --remote --merge deploy",
+		            "cd ../ && git status",
+		            "cd ../frontend && bower prune",
+		            "cd ../frontend && bower update",
+		            "cd ../frontend && grunt build:".$environment.($minify ? "" : " --nomin"),
+	                "chown -R nginx:nginx ../frontend/dist"];
 
-		COMMANDER::create()->zip(dirname(__DIR__)."/frontend/dist",["ignore"=>"/^\.git/"]);
-
-	} else {
-		if(!$branch)
-			$branch = shell_exec("cd ../ && git rev-parse --abbrev-ref HEAD");
-
-		$commands = [ 	is_os_windows() ? "cd" : "echo \$PWD",
-			            is_os_windows() ? "echo %PATH%" : "echo \$PWD",
-			            "git --version",
-			            "cd ../ && git fetch --all",
-			            "cd ../ && git reset --hard origin/".$branch,
-			            "cd ../ && git pull",
-			            "cd ../ && git submodule foreach --recursive git reset --hard",
-			            "cd ../ && git submodule update --init",
-			            "cd ../ && git submodule update --init --remote --merge deploy",
-			            "cd ../ && git status",
-			            "cd ../frontend && bower prune",
-			            "cd ../frontend && bower update",
-			            "cd ../frontend && grunt build:".$environment.($minify ? "" : " --nomin"),
-		                "chown -R nginx:nginx ../frontend/dist"];
-
-		if($environment=="development" || $environment=="staging") {
-	  		$commands[] = "cd ../backend/command && php upgrade.php";
-	   		$commands[] = "cd ../backend/command && php init.php";
-		}
-
-		COMMANDER::create()->run($commands,["title"=>"Building ".ucwords($environment)]);
+	if($environment=="development" || $environment=="staging") {
+  		$commands[] = "cd ../backend/command && php upgrade.php";
+   		$commands[] = "cd ../backend/command && php init.php";
 	}
 
+	if(preg_match("/build/",$action))
+		COMMANDER::create()->build($commands,["title"=>$title,"output"=>$output]);
+
+	if(preg_match("/zip/",$action))
+		COMMANDER::create()->zip(dirname(__DIR__)."/frontend/dist",["ignore"=>"/^\.git/"]);
 
 
--- deployngx.php
+++ deployngx.php
@@ -3,31 +3,28 @@
 
 	$environment	= value($_GET,"environment","dev");
 	$branch 		= value($_GET,"branch");
-	$output 		= value($_GET,"output");
+	$action			= value($_GET,"action","build");
+	$output			= $action=="build";
+	$title			= "Building ".ucwords($environment);
+	$branch 		= $branch ? $branch : shell_exec("cd ../ && git rev-parse --abbrev-ref HEAD");
 
-	if($output=="zip") {
+	$commands = [ 	is_os_windows() ? "cd" : "echo \$PWD",
+		            is_os_windows() ? "echo %PATH%" : "echo \$PATH",
+		            "cd ../ && git fetch --all",
+		            "cd ../ && git reset --hard origin/".$branch,
+		            "cd ../ && git pull",
+		            "cd ../ && git submodule foreach --recursive git reset --hard",
+		            "cd ../ && git submodule update --init",
+		            "cd ../ && git submodule update --init --remote --merge deploy",
+		            "cd ../ && git status",
+		            "cd ../backend/command && php upgrade.php",
+		            "cd ../backend/command && php init.php",
+		            "cd ../frontend && npm install",
+		            "cd ../frontend && ng build".($environment ? " --env=".$environment : "")."",
+	                "chown -R nginx:nginx ../frontend/dist" ];
 
+	if(preg_match("/build/",$action))
+		COMMANDER::create()->build($commands,["title"=>$title,"output"=>$output]);
+
+	if(preg_match("/zip/",$action))
 		COMMANDER::create()->zip(dirname(__DIR__)."/frontend/dist",["ignore"=>"/^\.git/"]);
-
-	} else {
-
-		if(!$branch)
-			$branch = shell_exec("cd ../ && git rev-parse --abbrev-ref HEAD");
-
-		$commands = [ 	is_os_windows() ? "cd" : "echo \$PWD",
-			            is_os_windows() ? "echo %PATH%" : "echo \$PATH",
-			            "cd ../ && git fetch --all",
-			            "cd ../ && git reset --hard origin/".$branch,
-			            "cd ../ && git pull",
-			            "cd ../ && git submodule foreach --recursive git reset --hard",
-			            "cd ../ && git submodule update --init",
-			            "cd ../ && git submodule update --init --remote --merge deploy",
-			            "cd ../ && git status",
-			            "cd ../backend/command && php upgrade.php",
-			            "cd ../backend/command && php init.php",
-			            "cd ../frontend && npm install",
-			            "cd ../frontend && ng build".($environment ? " --env=".$environment : "")."",
-		                "chown -R nginx:nginx ../frontend/dist" ];
-
-		COMMANDER::create()->run($commands,["title"=>"Building ".ucwords($environment)]);
-	}
