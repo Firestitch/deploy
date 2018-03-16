3b8319cfe9bf859a466d3777a8837e9710a8e40b
Raymond <ray@firestitch.com>
2018-03-15 21:04:57 -0400

    force submodule update

--- deploydemo.php
+++ deploydemo.php
@@ -4,21 +4,22 @@
     $repo = @$_GET["repo"];
     if(!$repo) {
         $payload = json_decode(value($_POST,"payload"));
-        $repo = preg_replace("/fs-/","",$payload->repository->name);
+        $repo = $payload->repository->name;
     }
+
+    $repo = preg_replace("/fs-/","",$repo);
 
     if(!$repo)
     	die("Failed to get repository name");
 
 	$commands = [  	is_os_windows() ? "cd" : "echo \$PWD",
 		            is_os_windows() ? "echo %PATH%" : "echo \$PATH",
-                    //"cd ../ && git fetch --all",
-		            //"cd ../ && git reset --hard origin/master",
-		            //"cd ../ && git pull",
-		            //"cd ../ && git submodule foreach --recursive git reset --hard origin/master",
-		            //"cd ../ && git submodule update --init",
-                    "cd ".$repo." && npm install",
-                    "cd ".$repo." && npm run demo",
+                    "cd ../ && git fetch --all",
+		            "cd ../ && git reset --hard origin/master",
+		            "cd ../ && git pull",
+		            "cd ../ && git submodule foreach --recursive git reset --hard origin/master",
+		            "cd ../ && git submodule update --remote --init",                    "cd ../".$repo." && npm install",
+                    "cd ../".$repo." && npm run demo:build",
                 	"chown -R nginx:nginx ../frontend/dist"];
 
-	COMMANDER::create()->build($commands,["title"=>"Building Demo","output"=>true]);
+	COMMANDER::create()->build($commands,["title"=>"Building ".ucfirst($repo)." Demo","output"=>true]);
