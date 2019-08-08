f0845ad524638ed7814ce7a49fa30fc8f31e61a8
Raymond Gigliotti <ray@firestitch.com>
2019-08-08 09:48:19 -0400

    added github email reporting

--- deploydemo.php
+++ deploydemo.php
@@ -1,24 +1,27 @@
 <?
 	require("__autoload.inc");
 
-    $repo = @$_GET["repo"];
+    $repo 			= @$_GET["repo"];
+    $payload 		= COMMANDER::get_github_payload();
 
-    if(!$repo) {
-        $repo = value(COMMANDER::get_github_payload(),["repository","name"]);
-        //file_put_contents("payload.json",value($_POST,"payload"));
-    }
+    if(!$repo) 
+        $repo = value($payload,["repository","name"]);
 
     $repo 			= preg_replace("/(fs-|ngx-|-)/","",$repo);
     $output_file 	= dirname(__DIR__)."/".$repo."/demo/index.html";
+    $github_email 	= value($payload,["pusher","email"],"");
+    $github_name 	= value($payload,["pusher","name"],"Unknown");	
 
     if(!$repo)
     	die("Failed to get repository name");
 
 	$commands = [  	is_os_windows() ? "cd" : "echo \$PWD",
 		            is_os_windows() ? "echo %PATH%" : "echo \$PATH",
+		            "echo \"GitHub User: $github_name $github_email\"",
                     "cd ../ && git fetch --all 2>&1",
 		            "cd ../ && git reset --hard 2>&1",
 		            "cd ../ && git pull 2>&1",
+		            "cd ../deploy && git reset --hard",
 		            "cd ../ && git submodule update deploy",
 		            "cd ../ && git submodule init ".$repo,
 		            "cd ../".$repo." && git rev-parse --abbrev-ref HEAD",
@@ -31,4 +34,5 @@
 	COMMANDER::create()->build($commands,[	"title"=>"Building ".ucfirst($repo)." Demo",
 											"output"=>true,
 											"output_file"=>$output_file,
+											"error_email"=>$github_email,
 											"process_key"=>$repo]);
