8c2887548d58936bda03ee08da02ffa5ef3c4f63
Raymond Gigliotti <ray@firestitch.com>
2020-01-13 10:51:39 -0500

    demo processes directory

--- deploydemo.php
+++ deploydemo.php
@@ -4,14 +4,17 @@
 	$payload 		= @json_decode(value($_POST,"payload"));
 	$github_email 	= value($payload,["pusher","email"],"");
 	$repo			= value($_GET,"repo",value($payload,["repository","name"]));
-    $repo 			= preg_replace("/(fs-|ngx-|-)/","",$repo);
+    $repo 			= preg_replace("/(fs-|ngx-|-)/","",$repo);       	
 
     if(!$repo)
-    	die("Failed to get repository name");
+		die("Failed to get repository name");
 
-	$config = [
-		"repo"=>$repo
-	];
+    $config = [
+            "repo"=>$repo
+    ];
 
-	run_process("deploydemo-process.php", $config, $github_email, $repo."/process.pid");
+    @mkdir("processes");
+	run_process("deploydemo-process.php", $config, $github_email, "processes/".$repo.".pid");
+
+
 
