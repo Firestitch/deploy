edce613ced10760d2e1caf35d8280f83ac4c5554
Raymond Gigliotti <ray@firestitch.com>
2020-05-28 11:54:06 -0400

    ignore if error is empty

--- commander/commander.template.php
+++ commander/commander.template.php
@@ -3,7 +3,7 @@
 	<head>
 	    <meta charset="UTF-8">
 		<style>
-			<?=file_get_contents(dirname(__FILE__)."/styles.css") ?>
+			<?=file_get_contents(dirname(__FILE__) . "/styles.css")?>
 		</style>
 	</head>
 	<body>
@@ -12,62 +12,65 @@
 		<h2>Built on <?=date("F j, Y, g:i a e")?></h2>
 
 		<div class="output">
-			<? foreach($commands as $command) { ?>
+			<?foreach ($commands as $command) {
+	?>
 
 		        <span class="prompt">$</span> <span class="command"><?=$command?></span>
 
 		        <?
-		        	$this->flush();
+	$this->flush();
 
-					$descriptorspec = array(
-						0 => ["pipe", "r"],
-						1 => ["pipe", "w"],
-						2 => ["pipe", "w"]
-					);
+	$descriptorspec = array(
+		0 => ["pipe", "r"],
+		1 => ["pipe", "w"],
+		2 => ["pipe", "w"],
+	);
 
-					$this->flush();
-					$process = proc_open($command, $descriptorspec, $pipes, realpath('./'));
-					@fclose($pipes[0]);
+	$this->flush();
+	$process = proc_open($command, $descriptorspec, $pipes, realpath('./'));
+	@fclose($pipes[0]);
 
-					echo "<pre>";
+	echo "<pre>";
 
-		          	if(is_resource($process)) {
+	if (is_resource($process)) {
 
-				      	while($string=fgets($pipes[1])) {
-							echo trim($converter->convert($string));
-							$this->flush();
-				        }
+		while ($string = fgets($pipes[1])) {
+			echo trim($converter->convert($string));
+			$this->flush();
+		}
 
-				      	while($string=fgets($pipes[2])) {
-				      		$string = trim($converter->convert($string));
-				          	echo "<div class=\"error\">$string</div>";
-				          	$this->flush();
-				          	$errors[] = $string;
-				        }
-			        }
-			        
-		          	echo "</pre>";
+		while ($string = fgets($pipes[2])) {
+			$string = trim($converter->convert($string));
+			if ($string) {
+				echo "<div class=\"error\">$string</div>";
+				$this->flush();
+				$errors[] = $string;
+			}
+		}
+	}
 
-		        	@fclose($pipes[1]);
-		         	@fclose($pipes[2]);
-		          	proc_close($process);
-		        ?>
+	echo "</pre>";
 
-		        <? $this->flush() ?>
+	@fclose($pipes[1]);
+	@fclose($pipes[2]);
+	proc_close($process);
+	?>
 
-		        <? 
-		        	if($errors) {
-		        		break;
-		        	}
-		        ?>
-			<? } ?>
+		        <?$this->flush()?>
+
+		        <?
+	if ($errors) {
+		break;
+	}
+	?>
+			<?}?>
 		</div>
 
-		<? if($errors) { ?>
+		<?if ($errors) {?>
 			<h1 class="error" error="false">Build Failed</h1>
-		<? } else { ?>
+		<?} else {?>
 			<h1 class="success">Build Complete</h1>
-		<? } ?>
+		<?}?>
 
 	</body>
 </html>
