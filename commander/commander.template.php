71b4dabfd3794576239adb881ddaab55a2600d52
Ray <ray@email.com>
2021-09-10 13:49:18 -0400

    moved body style to inner div

--- commander/commander.template.php
+++ commander/commander.template.php
@@ -12,69 +12,69 @@ $errors = [];
 </head>
 
 <body>
+	<div class="body-container">
+		<h1><?php echo $title ?></h1>
+		<h2>Built on <?php echo date("F j, Y, g:i a e") ?></h2>
 
-	<h1><?php echo $title ?></h1>
-	<h2>Built on <?php echo date("F j, Y, g:i a e") ?></h2>
+		<div class="output">
+			<?php foreach ($commands as $command) { ?>
 
-	<div class="output">
-		<?php foreach ($commands as $command) { ?>
+				<span class="prompt">$</span> <span class="command"><?php echo $command ?></span>
 
-			<span class="prompt">$</span> <span class="command"><?php echo $command ?></span>
+				<?php
+				$self->flush();
 
-			<?php
-			$self->flush();
+				$descriptorspec = array(
+					0 => ["pipe", "r"],
+					1 => ["pipe", "w"],
+					2 => ["pipe", "w"],
+				);
 
-			$descriptorspec = array(
-				0 => ["pipe", "r"],
-				1 => ["pipe", "w"],
-				2 => ["pipe", "w"],
-			);
+				$self->flush();
+				$process = proc_open($command, $descriptorspec, $pipes, realpath('./'));
+				@fclose($pipes[0]);
 
-			$self->flush();
-			$process = proc_open($command, $descriptorspec, $pipes, realpath('./'));
-			@fclose($pipes[0]);
+				echo "<pre>";
 
-			echo "<pre>";
+				if (is_resource($process)) {
 
-			if (is_resource($process)) {
-
-				while ($string = fgets($pipes[1])) {
-					echo trim($converter->convert($string));
-					$self->flush();
-				}
-
-				while ($string = fgets($pipes[2])) {
-					if (trim($string)) {
-						$string = trim($converter->convert($string));
-						echo "<div class=\"error\">$string</div>";
+					while ($string = fgets($pipes[1])) {
+						echo trim($converter->convert($string));
 						$self->flush();
-						$errors[] = $string;
 					}
+
+					while ($string = fgets($pipes[2])) {
+						if (trim($string)) {
+							$string = trim($converter->convert($string));
+							echo "<div class=\"error\">$string</div>";
+							$self->flush();
+							$errors[] = $string;
+						}
+					}
+
+					$exitcode = value(proc_get_status($process), "exitcode");
+
+					if ($exitcode > 0)
+						$self->_failed = true;
 				}
 
-				$exitcode = value(proc_get_status($process), "exitcode");
+				echo "</pre>";
+				@fclose($pipes[1]);
+				@fclose($pipes[2]);
+				proc_close($process);
+				?>
 
-				if ($exitcode > 0)
-					$self->_failed = true;
-			}
+				<?php $self->flush() ?>
 
-			echo "</pre>";
-			@fclose($pipes[1]);
-			@fclose($pipes[2]);
-			proc_close($process);
-			?>
+			<?php } ?>
+		</div>
 
-			<?php $self->flush() ?>
-
+		<?php if ($self->_failed) { ?>
+			<h1 class="error" error="false">Build Failed</h1>
+		<?php } else { ?>
+			<h1 class="success">Build Successful</h1>
 		<?php } ?>
 	</div>
-
-	<?php if ($self->_failed) { ?>
-		<h1 class="error" error="false">Build Failed</h1>
-	<?php } else { ?>
-		<h1 class="success">Build Successful</h1>
-	<?php } ?>
-
 </body>
 
 </html>
