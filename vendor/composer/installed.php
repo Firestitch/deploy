d387b4d97497ec3efd30cf5640cb7f902af23911
Raymond Gigliotti <ray@firestitch.com>
2022-10-25 10:03:23 -0400

    updated PHPstan

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -6,7 +6,7 @@
     'aliases' => 
     array (
     ),
-    'reference' => '5ac6a1588d315e37cbf88a4d429aa22a04f898d0',
+    'reference' => '399d4e84630d86190e14fddf9884edcbd4d124fc',
     'name' => '__root__',
   ),
   'versions' => 
@@ -18,7 +18,7 @@
       'aliases' => 
       array (
       ),
-      'reference' => '5ac6a1588d315e37cbf88a4d429aa22a04f898d0',
+      'reference' => '399d4e84630d86190e14fddf9884edcbd4d124fc',
     ),
     'miladrahimi/phpcrypt' => 
     array (
@@ -31,21 +31,21 @@
     ),
     'phpmailer/phpmailer' => 
     array (
-      'pretty_version' => 'v6.6.4',
-      'version' => '6.6.4.0',
+      'pretty_version' => 'v6.6.5',
+      'version' => '6.6.5.0',
       'aliases' => 
       array (
       ),
-      'reference' => 'a94fdebaea6bd17f51be0c2373ab80d3d681269b',
+      'reference' => '8b6386d7417526d1ea4da9edb70b8352f7543627',
     ),
     'phpstan/phpstan' => 
     array (
-      'pretty_version' => '1.8.5',
-      'version' => '1.8.5.0',
+      'pretty_version' => '1.8.11',
+      'version' => '1.8.11.0',
       'aliases' => 
       array (
       ),
-      'reference' => 'f6598a5ff12ca4499a836815e08b4d77a2ddeb20',
+      'reference' => '46e223dd68a620da18855c23046ddb00940b4014',
     ),
     'sensiolabs/ansi-to-html' => 
     array (
